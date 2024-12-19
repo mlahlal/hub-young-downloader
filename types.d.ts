0f6e137cc20006e9af4928ef98a33bd90cd192cf
Mouad Lahlal <lahlalmouad@gmail.com>
2024-12-19 11:43:27 +0100

    Fixed minor issues

--- types.d.ts
+++ types.d.ts
@@ -1,12 +1,14 @@
-export interface Book {
-	name: string;
-	value: string;
-	thumbnail: string;
-}
+declare module "hub-young-downloader" {
+	export interface Book {
+		name: string;
+		value: string;
+		thumbnail: string;
+	}
 
-export class HubYoung {
-	constructor(): void;
-	login(username: string, psw: string): void;
-	getBooks(): Array<Book>;
-	download(volumeId: string): void;
+	export class HubYoung {
+		constructor(): void;
+		login(username: string, psw: string): void;
+		getBooks(): Array<Book>;
+		download(volumeId: string): void;
+	}
 }
