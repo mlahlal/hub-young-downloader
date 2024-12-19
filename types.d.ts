1856c514d753892db69fabb50ce5bb14969c74ec
Mouad Lahlal <lahlalmouad@gmail.com>
2024-12-19 11:24:47 +0100

    Added types

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+export interface Book {
+	name: string;
+	value: string;
+	thumbnail: string;
+}
+
+export class HubYoung {
+	constructor(): void;
+	login(username: string, psw: string): void;
+	getBooks(): Array<Book>;
+	download(volumeId: string): void;
+}
